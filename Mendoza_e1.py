3360a7746389029833e32fbb8a5d96c330c52233
Janine Mendoza <mendozajanine43@gmail.com>
2018-10-25 22:24:26 +0800

    Exercise No.1

---
+++ Mendoza_e1.py
@@ -0,0 +1,11 @@
+data = {"Name":"Janine C. Mendoza",
+"My Spirit Animal":"Cat",
+"Why":"I can be left Alone",
+"Hobby":"Eating and Sleeping",
+"Profession":"Engineer"}
+#print(data)
+print("I am {} ".format(data["Name"]))
+print("My spirit Animal is {}".format(data["My Spirit Animal"]))
+print("Because {}".format(data["Why"]))
+print("When not in school, {}".format(data["Hobby"]))
+print("I dream of being a/an {}".format(data["Profession"]))
--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
 print("Hello World!")
 print("My name is {}, I am {} y/o."
-.format("Janine", 17))
+    .format("Janine",17))
+print("AdNU", 2018, sep="-",end="")
+print("CSNHS")
